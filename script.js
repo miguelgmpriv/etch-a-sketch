6ef88bc7cdd0c5a7a6f8d850a62992a1f25857ea
Miguel <miguelgm.priv@gmail.com>
2021-08-23 03:08:47 -0700

    Cleaned up code and added more comments

--- script.js
+++ script.js
@@ -2,34 +2,39 @@
 let pixels = [];
 let sketchBoard = document.querySelector('#sketchBoard');
 var root = document.querySelector(':root');
-let option = 0;
+let option = 0; //global variable to switch on or off random color
 
-document.getElementById('pixelSlider').addEventListener('change', function(e){
-    //user selected pixel size used here to start setting up the board
-    setBoard(e.target.value); 
-    drawBoard(e.target.value,e.target.value); 
-});
-
-document.getElementById('colorPicker').addEventListener("change", function(e){
-    //gets the value from color input and updates css variable
-    root.style.setProperty('--backgroundColor', e.target.value);
-    option = 0;
-});
-
-document.getElementById('clear').addEventListener('click',function(){
-    //"clears" the board by changing all the pixel class background to white.
-    let pixs = document.querySelectorAll('.pixel');
-    pixs.forEach(pixel => pixel.style.backgroundColor = 'white');
-});
-
-document.getElementById('eraser').addEventListener('click', options); //begin "eraser" thread
-
+//All event listeners to go to options or changePixel functions
+document.getElementById('pixelSlider').addEventListener('change', options);
+document.getElementById('colorPicker').addEventListener("change", options);
+document.getElementById('clear').addEventListener('click', options);
+document.getElementById('eraser').addEventListener('click', options); 
 document.getElementById('rgb').addEventListener('click', options);
-
 document.getElementById('sketchBoard').addEventListener('mouseover', changePixel);
 
-function setBoard(pixChoice){
+function options(event){
+    if (event.target.id === 'eraser'){
+        //Set up "eraser" by changing mouse trail to replace background color to white
+        root.style.setProperty('--backgroundColor', `white`);
+        option = 0;
+    } else if (event.target.id === 'rgb'){
+        option = 1;
+    } else if (event.target.id === 'colorPicker'){
+        //Change css variable to user selected color
+        root.style.setProperty('--backgroundColor', e.target.value);
+        option = 0;
+    } else if (event.target.id === 'clear'){
+        //Clear the board by changing each pixel background to white
+        let pixs = document.querySelectorAll('.pixel');
+        pixs.forEach(pixel => pixel.style.backgroundColor = 'white'); 
+    } else if (event.target.id === 'pixelSlider'){
+        //Redraw board based on user input
+        setBoard(e.target.value); 
+        drawBoard(e.target.value,e.target.value); 
+    }
+}
 
+function setBoard(pixChoice){
     while(sketchBoard.firstChild){
         sketchBoard.removeChild(sketchBoard.firstChild);
     } //removes grid
@@ -42,34 +47,22 @@ function setBoard(pixChoice){
 function drawBoard(pix, maxPixels){
     //use recursion to draw the board
     if (pix === 0) return;
-    
     drawBoard(pix - 1,maxPixels);
-    
     for(let i = 0; i < maxPixels; i++){
         pixels[i] = document.createElement("div");
         sketchBoard.append(pixels[i]);
         pixels[i].classList.add("pixel");
     }
 }
-function options(event){
-    if (event.target.id === 'eraser'){
-        root.style.setProperty('--backgroundColor', `white`);
-        option = 0;
-    } else if (event.target.id === 'rgb'){
-        option = 1;
-    }
-}
 
 function changePixel(event){
     if (option === 1) {
+        //activate random if option equals 1
         let rgb = Math.floor(Math.random()*16777215).toString(16);
         root.style.setProperty('--backgroundColor', `#${rgb}`);
     } 
+    //use css variable to replace target background color
     event.target.style.backgroundColor = getComputedStyle(root).getPropertyValue('--backgroundColor');            
 }
-
-window.onload = function(){
-    //default board settings
-    setBoard(50);
-    drawBoard(50,50);
-};
+//Setups default settings on load
+window.onload = () => {setBoard(50);drawBoard(50,50);};
