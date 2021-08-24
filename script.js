fdb5625c6964455ff2219588bf31d0ceadb9a47d
Miguel <miguelgm.priv@gmail.com>
2021-08-23 17:59:46 -0700

    Added css and finished up the code

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
-let pixels = [];
-let sketchBoard = document.querySelector('#sketchBoard');
-var root = document.querySelector(':root');
-let option = 0; //global variable to switch on or off random color
+let pixels = []; //Empty array for our board
+const sketchBoard = document.querySelector('#sketchBoard');
+const root = document.querySelector(':root');
+let option = 0; //Global variable to switch on or off random color
 
 //All event listeners to go to options or changePixel functions
 document.getElementById('pixelSlider').addEventListener('change', options);
@@ -14,23 +13,25 @@ document.getElementById('sketchBoard').addEventListener('mouseover', changePixel
 
 function options(event){
     if (event.target.id === 'eraser'){
-        //Set up "eraser" by changing mouse trail to replace background color to white
+        //Set up "eraser" by replacing background color to white
         root.style.setProperty('--backgroundColor', `white`);
         option = 0;
     } else if (event.target.id === 'rgb'){
         option = 1;
     } else if (event.target.id === 'colorPicker'){
         //Change css variable to user selected color
-        root.style.setProperty('--backgroundColor', e.target.value);
+        root.style.setProperty('--backgroundColor', event.target.value);
         option = 0;
     } else if (event.target.id === 'clear'){
         //Clear the board by changing each pixel background to white
         let pixs = document.querySelectorAll('.pixel');
         pixs.forEach(pixel => pixel.style.backgroundColor = 'white'); 
     } else if (event.target.id === 'pixelSlider'){
-        //Redraw board based on user input
-        setBoard(e.target.value); 
-        drawBoard(e.target.value,e.target.value); 
+        //Display pixels
+        const pixelSize = document.getElementById('pixelSize')
+        pixelSize.textContent = `${event.target.value} X ${event.target.value}`;
+        //Redraw board based on user input        
+        setBoard(event.target.value);
     }
 }
 
@@ -38,10 +39,10 @@ function setBoard(pixChoice){
     while(sketchBoard.firstChild){
         sketchBoard.removeChild(sketchBoard.firstChild);
     } //removes grid
-
     let percent = 100/pixChoice; // used to set up board based on percentage
     root.style.setProperty('--pixelHeight', `${percent}%`);
     root.style.setProperty('--pixelWidth', `${percent}%`);
+    drawBoard(pixChoice, pixChoice)
 }
 
 function drawBoard(pix, maxPixels){
@@ -57,12 +58,13 @@ function drawBoard(pix, maxPixels){
 
 function changePixel(event){
     if (option === 1) {
-        //activate random if option equals 1
+        //Activate random if option equals 1
         let rgb = Math.floor(Math.random()*16777215).toString(16);
         root.style.setProperty('--backgroundColor', `#${rgb}`);
     } 
     //use css variable to replace target background color
     event.target.style.backgroundColor = getComputedStyle(root).getPropertyValue('--backgroundColor');            
 }
+
 //Setups default settings on load
-window.onload = () => {setBoard(50);drawBoard(50,50);};
+window.onload = setBoard(50);
